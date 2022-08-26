cc1bde6a72145f5dea35fe95b474ac235fcf8518
Brian Takita <brian.takita@gmail.com>
2022-08-26 18:06:13 -0500

    GlobalStyle: fix: reference to Style

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,4 +1,5 @@
 import { createEffect, createMemo, createSignal, type ParentComponent } from 'solid-js'
+import { Style } from 'solid-start'
 export const GlobalStyle:ParentComponent<Style_props_T> = $p=>{
 	const [ref_, ref__set] = createSignal<HTMLStyleElement>()
 	const css_ = createMemo(()=>$p.css || $p.children)
@@ -8,10 +9,10 @@ export const GlobalStyle:ParentComponent<Style_props_T> = $p=>{
 			ref.innerHTML = css_()
 		}
 	})
-	return <style ref={$=>{
+	return <Style ref={$=>{
 		ref__set($)
 		if ($p.ref) $p.ref($)
-	}} data-label={$p.label}>{css_()}</style>
+	}} data-label={$p.label}>{css_()}</Style>
 }
 export type Style_props_T = {
 	css?:string
